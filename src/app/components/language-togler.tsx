24b9751841e4895d6f15d9bb9eb4d6471523c3ce
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-01-28 14:05:27 +0200

    made language button look nice

--- src/app/components/language-togler.tsx
+++ src/app/components/language-togler.tsx
@@ -19,13 +19,16 @@ export default function LanguageToggle({ initialLocale }: { initialLocale: strin
     return (
         <button
             onClick={toggleLanguage}
-            className="flex items-center gap-2 px-4 py-2 bg-white border border-slate-200 rounded-full shadow-sm hover:bg-slate-50 transition-colors text-sm font-medium text-slate-700"
+            className="flex items-center gap-2 px-4 py-2 bg-black-900 border-solid border-1 border-primary rounded-full shadow-sm transition-colors text-sm font-medium text-gray-100"
         >
-            <Languages size={16} className="text-blue-500" />
+            <Languages
+                size={16}
+                className="text-primary"
+            />
             <span>
                 {locale === 'en-US' ? 'English' : 'Română'}
             </span>
-            <span className="text-xs text-slate-400 border-l pl-2 ml-1">
+            <span className="text-xs text-gray-100 border-l pl-2 ml-1">
                 {locale === 'en-US' ? 'RO' : 'EN'}
             </span>
         </button>
