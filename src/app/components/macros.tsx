dfcae2ea3143975e12bbaa493e4464d9192cb98c
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-02-01 20:13:52 +0200

    improve padding

--- src/app/components/macros.tsx
+++ src/app/components/macros.tsx
@@ -14,7 +14,7 @@ export function Macros({macros}: { macros: string }) {
                 overflow-hidden transition-all duration-300 ease-out
                 ${open ? 'max-h-20 opacity-100 mt-2' : 'max-h-0 opacity-0'}
                 `}>
-                <p className="text-center text-xs mt-2">{macros}</p>
+                <p className="text-center text-xs mt-2 px-2">{macros}</p>
             </div>
         </div>
     );
